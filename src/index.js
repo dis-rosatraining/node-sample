5c7d5fbafe59e5fb3d2e94ef25fb86b3ce845b95
dis-rosatraining <dis.rosatraining@gmail.com>
2023-06-17 13:05:54 -0400

    1st

---
+++ src/index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+
+var today = new Date();
+
+var month = today.getMonth()+1;
+var day = today.getDate();
+
+app.get("/",function(req,res){
+    res.send(month + '月'+ day + '日 ' + 'Willcome DiS わぁるど')  
+    res.send()
+})
+
+app.listen(3000, () => console.log('Listening on port 3000'));
+
