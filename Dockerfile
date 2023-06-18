c1dc6ef74e31409dbb46dc8f96e4dfb43ac5d6b3
dis-rosatraining <dis.rosatraining@gmail.com>
2023-06-17 21:03:03 -0400

    2nd

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM node:18.12.0-alpine3.16
 WORKDIR /app
 COPY . .
+RUN npm install -g npm@9.7.1
 RUN npm install --save express
 CMD ["node", "src/index.js"]
 USER 1001
--- src/index.js
+++ src/index.js
@@ -13,5 +13,5 @@ app.get("/",function(req,res){
     res.send()
 })
 
-app.listen(3000, () => console.log('Listening on port 3000'));
+app.listen(3000, '0.0.0.0', () => console.log('Listening on port 3000'));
 
